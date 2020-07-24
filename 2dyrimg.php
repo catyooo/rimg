2751af9c429f385fbd4ea3076c710bc5dbe3984a
catyooo <66102273+catyooo@users.noreply.github.com>
2020-07-24 15:14:51 +0800

    rimg

---
+++ 2dyrimg.php
@@ -0,0 +1,59 @@
+<?php
+// 存储数据的文件
+$filename = '2dysinetxt.txt';
+if(!file_exists($filename)) {
+    die($filename . ' 数据文件不存在');
+}
+// 读取整个数据文件
+$data = file_get_contents($filename);
+// 按换行符分割成数组
+$data = explode(PHP_EOL, $data);
+// 随机获取一行索引
+$result = $data[array_rand($data)];
+// 去除多余的换行符（解决获取空值问题）
+$result = str_replace(array("\r","\n","\r\n"), '', $result);
+$size_arr = array('large', 'mw1024', 'mw690', 'bmiddle', 'small', 'thumb180', 'thumbnail', 'square');
+$size = !empty($_GET['size']) ? $_GET['size'] : 'large' ;
+$server = rand(1,4);
+if(!in_array($size, $size_arr)){
+	$size = 'large';
+}
+$url = 'https://tva'.$server.'.sinaimg.cn/'.$size.'/'.$result.'.jpg';
+//解析JSON
+$result=array("code"=>"200","acgurl"=>"$url");
+$type=$_GET['return'];
+switch ($type)
+{   
+//格式解析                             
+case 'json':
+$path = "$url";
+$pathinfo = pathinfo($path);
+$imageInfo = getimagesize($url);  
+$result['width']="$imageInfo[0]";  
+$result['height']="$imageInfo[1]";
+$result['size']="$pathinfo[extension]"; 
+header('Content-type:text/json');
+$url=str_replace("http:","https:", $result);
+echo json_encode($url);
+break;
+//不输出图片链接直接显示                             
+case 'img':
+$img = file_get_contents($url,true);
+header("Content-Type: image/jpeg;");
+echo $img;
+break;
+//IMG
+default:
+header("Location:".$result['acgurl']);
+break;
+}
+$type1=$_GET['ssl'];
+switch ($type1)
+{   
+//HTTPS图片输出                             
+case 'true':
+$url=str_replace("http:","https:", $result['acgurl']);
+header("Location:".$url);
+break;
+}
+?>
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # rimg
  acgimgurl 修改优化版
+
+调用地址:https://api.catyo.cn/rimg/2dyrimg.php
+调用参数:return=json/img
+json：返回标准json数据(图片地址)
+img：直接php显示图片不返回图片链接 ssl=true(开启https跳转)
+
+请勿用于商业用途 转载请加出处 多谢合作~ 程序自带600多张图片
+
+
+原作者https://www.toubiec.cn/
+
+注：如需要原版持续，更新请关注苏晓晴博客或者原作者的acgimgurl仓库https://github.com/Suxiaoqinx/acgimgurl
